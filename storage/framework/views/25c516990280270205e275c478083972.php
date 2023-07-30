12177ce5001b6ce9864ecb220e65cde4c663b719
RamaPrihandana <133082257+RamaPrihandana@users.noreply.github.com>
2023-07-30 10:52:15 +0700

    Add files via upload

---
+++ storage/framework/views/25c516990280270205e275c478083972.php
@@ -0,0 +1,60 @@
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div class="container">
+        <a class="navbar-brand" href="<?php echo e(url('/home')); ?>">
+            <?php echo e(config('app.name', 'Laravel')); ?>
+
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="<?php echo e(__('Toggle navigation')); ?>">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+
+            <hr class="d-md-none text-white-50">
+            <ul class="navbar-nav flex-row flex-wrap">
+                <li class="nav-item col-2 col-md-auto"><a href="<?php echo e(route('home')); ?>"
+                        class="nav-link <?php if($currentRouteName == 'home'): ?> active <?php endif; ?>">Home</a></li>
+                <li class="nav-item col-2 col-md-auto"><a href="<?php echo e(route('employees.index')); ?>"
+                        class="nav-link <?php if($currentRouteName == 'employees.index'): ?> active <?php endif; ?>">Employee</a></li>
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ms-auto">
+                <!-- Authentication Links -->
+                <?php if(auth()->guard()->guest()): ?>
+                    <?php if(Route::has('login')): ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo e(route('login')); ?>"><?php echo e(__('Login')); ?></a>
+                        </li>
+                    <?php endif; ?>
+                <?php else: ?>
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <?php echo e(Auth::user()->name); ?>
+
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a href="<?php echo e(route('profile')); ?>" class="dropdown-item"><i class="bi-person-circle me-1"></i> My
+                                Profile</a>
+                            <a class="dropdown-item" href="<?php echo e(route('logout')); ?>"
+                                onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                <?php echo e(__('Logout')); ?>
+
+                            </a>
+
+                            <form id="logout-form" action="<?php echo e(route('logout')); ?>" method="POST" class="d-none">
+                                <?php echo csrf_field(); ?>
+                            </form>
+                        </div>
+                    </li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </div>
+</nav>
+<?php /**PATH C:\Users\Muhamad Brilliant\modul4\resources\views/layouts/nav.blade.php ENDPATH**/ ?>
